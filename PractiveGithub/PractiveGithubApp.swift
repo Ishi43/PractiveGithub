4b391806db23afdfd8ffa3b7961a696eca6526e2
Ishika <ishika.meel.17@gmail.com>
2023-10-29 15:00:38 -0400

    Initial Commit

---
+++ PractiveGithub/PractiveGithubApp.swift
@@ -0,0 +1,17 @@
+//
+//  PractiveGithubApp.swift
+//  PractiveGithub
+//
+//  Created by Ishika Meel on 10/29/23.
+//
+
+import SwiftUI
+
+@main
+struct PractiveGithubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
